ff920c4759330e93899dfd8225f0b3348b8b3cde
Sean Remenyi <sean.remenyi@gmail.com>
2021-05-31 19:57:56 +1000

    logic finished

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-def coff_machine():
-    machine = CoffeeMaker()
-    choices = Menu()
+machine = CoffeeMaker()
+choices = Menu()
+money_counter = MoneyMachine()
 
-    while True:
-        user_input = input("What would you like? (espresso/latte/cappuccino): "):
-        if user_input == "off":
-            break
-        elif
+is_on = True
+
+while is_on:
+    options = choices.get_items()
+    user_input = input("What would you like? (espresso/latte/cappuccino): ")
+    if user_input == "off":
+        is_on = False
+    elif user_input == "report":
+        machine.report()
+        money_counter.report()
+    else:
+        item = choices.find_drink(user_input)
+        resources = machine.is_resource_sufficient(item)
+        payment = money_counter.make_payment(item.cost)
+        if payment and resources:
+            machine.make_coffee(item)
